f36c5a7d2b1e4bdf1b97643a2d44683612d55e39
Elie MEN <133395047+Elimen210@users.noreply.github.com>
2024-07-23 18:37:21 +0100

    commit ;)

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,15 +1,28 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const DASH_VELOCITY = 30.0
 const SPEED_VELOCITY = -10.0
-
+@export var mouse_sensitivity = 0.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var camera := $Neck/Camera3D
+@onready var neck := $Neck
 
-
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	$Area3D.body_entered.connect(start_dialog)
+	
+func _unhandled_input(event):
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			neck.rotate_y(-event.relative.x * 0.05)
+			camera.rotate_x(-event.relative.x * 0.05)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
+			
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -27,14 +40,19 @@ func _physics_process(delta):
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		#dash
-		
+		if Input.is_action_just_pressed("insert"):
+			velocity.x *= DASH_VELOCITY
 		#sprint
 		if Input.is_action_just_pressed("sprint"):
 			velocity.x = SPEED_VELOCITY
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	if Input.is_action_just_pressed("insert"):
-			velocity.x *= DASH_VELOCITY
+	
+func start_dialog(_body):
+	var scene := preload("res://INTERAGIR.tscn")
+	var dialog = scene.instantiate()
+	add_child(dialog)
+	
 
 	move_and_slide()
