38a9300e69792f01a0987646245b30f6ffc04494
Elie MEN <133395047+Elimen210@users.noreply.github.com>
2024-07-25 18:24:03 +0100

    weapon commit
    I gave a weapon to the player also I animated it

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -9,32 +9,25 @@ const SPEED_VELOCITY = -10.0
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var camera := $Neck/Camera3D
 @onready var neck := $Neck
+@onready var anim_player = $AnimationPlayer
+@onready var hitbox = $WeaponSlot/MeshInstance3D2/hitbox
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	$Area3D.body_entered.connect(start_dialog)
 	
-func _unhandled_input(event):
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			neck.rotate_y(-event.relative.x * 0.05)
-			camera.rotate_x(-event.relative.x * 0.05)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
-			
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_up", "ui_down", "ui_right", "ui_left")
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -42,6 +35,9 @@ func _physics_process(delta):
 		#dash
 		if Input.is_action_just_pressed("insert"):
 			velocity.x *= DASH_VELOCITY
+		if Input.is_action_just_pressed("insert") && Input.is_action_just_pressed("ui_up"):
+			velocity.z *= DASH_VELOCITY
+		
 		#sprint
 		if Input.is_action_just_pressed("sprint"):
 			velocity.x = SPEED_VELOCITY
@@ -49,10 +45,21 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
-func start_dialog(_body):
-	var scene := preload("res://INTERAGIR.tscn")
-	var dialog = scene.instantiate()
-	add_child(dialog)
 	
-
+func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		anim_player.play("attack")
+		hitbox.monitoring = true
+	
 	move_and_slide()
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "attack":
+		anim_player.play("idle")
+		hitbox.monitoring = false
+
+
+func _on_hitbox_area_entered(area):
+	if area.is_in_group("ennemy"):
+		print("Ennemy hit F*ck !!!!")
